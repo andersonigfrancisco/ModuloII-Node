d8649a9d7aa0793ca397e57a14f530099b898c4e
andersonigfrancisco <andersonfranciscoig@gmail.com>
2022-08-18 12:46:27 +0100

    refactor my project

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,10 @@
 import express from 'express';
 
-import { createCourse } from './router';
 
 const app = express();
 
 app.use(express.json())
 
-app.get('/',createCourse)
+
 
 app.listen(3001,() => console.log("server is running!"));
