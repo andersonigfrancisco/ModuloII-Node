925820dc3185baca9c30eee0600e6de45c8dfdf9
andersonigfrancisco <andersonfranciscoig@gmail.com>
2022-08-22 16:37:40 +0100

    create respositoriesSpecification

---
+++ src/modules/cars/repositories/SpecificationRepositor.ts
@@ -0,0 +1,29 @@
+import { Specification } from "../model/Specification";
+import { ISpecificationRepositor,ICreateSpecificationDTO } from "./ISpecificationRepositor";
+
+
+
+
+
+export class SpecificationRepositor implements ISpecificationRepositor{
+
+  private specification:Specification[];
+
+  constructor(){
+    this.specification = [];
+  }
+
+  create({name,description}:ICreateSpecificationDTO):void{
+
+    const categorie = new Specification();
+
+    Object.assign(categorie,{
+      name,
+      description,
+      created_at:new Date()
+    })
+  
+    this.specification.push(categorie)
+
+  }
+}
